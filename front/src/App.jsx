13424077486fae0b81d5075547c7229e533f3dd0
Mohamed Baatour <mohamedbaatourx@gmail.com>
2025-01-15 19:52:09 +0100

    final commit V2

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -23,6 +23,8 @@ function App() {
   
    const formRef = useRef(null)
 
+  const productRef = useRef(null)
+
 
     const fetchMustangs = async () => {
       try {
@@ -135,8 +137,8 @@ window.location.reload()
         <nav>
       <img className="logo" src="https://api.iconify.design/mdi:car-turbocharger.svg?color=%23cccccc" alt="ToolVault" />
       <ul className="nav-links">
-        <li><a onClick={() => formRef.current.scrollIntoView({ behavior: 'smooth' })} href="#" >Sell A Car</a></li>
-        <li><a href="#">Models</a></li>
+        <li><a onClick={() => formRef.current.scrollIntoView({ behavior: "smooth" })} href="#" >Sell A Car</a></li>
+        <li><a onClick={() => productRef.current.scrollIntoView({behavior: "smooth"})}  href="#">Models</a></li>
         <li><a href="https://github.com/mohamedbaatour" target="_blank">About Us</a></li>
       </ul>
       <button className="search-input-nav">Search...</button>
@@ -149,7 +151,7 @@ window.location.reload()
         <h1 className='h1'>Unleash the Power - Find Your Dream Sports Car Today!</h1>
         <p className='h1-p'>filter , add cars for sale, and much more! <br></br>
          </p>
-        <div className='search'>
+        <div ref={productRef} className='search'>
           <svg className='search-icon' xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#cccccc" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"/></svg>
           <input onChange={handleSearch} className='search-input' type='search' placeholder='SEARCH...'></input>
         </div>
@@ -159,7 +161,7 @@ window.location.reload()
           <button className={current === 'bmwm' ? 'showBMW active': 'showBMW'} onClick={() => setCurrent("bmwm")}>Show BMWs</button>
         </div>
 
-        <h2 className='fords'>{changeH2()}</h2>
+        <h2  className='fords'>{changeH2()}</h2>
         <div className='cards'>
         {filteredData.map((mustang) => {
           return (
